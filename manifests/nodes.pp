61c56dbdad8d15cc7e4c8a6ae975b924cf3ffbde
Dark src himself <nishantrayan@gmail.com>
2014-10-09 15:44:38 +0000

    making it work in demo2

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,3 +1,7 @@
 node 'demo' {
   include nginx
 }
+
+node 'demo2' {
+  include nginx
+}
--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,1 @@
 import 'nodes.pp'
-include 'git'
-git::config { 'user.name':
-  value => 'Dark src himself',
-}
-
-git::config { 'user.email':
-  value => 'nishantrayan@gmail.com',
-}
